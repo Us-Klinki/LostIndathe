0cc616ea97ad22055ba688baf36a9561ed7e07f3
Tom <Tom@Lalilula.fritz.box>
2024-09-28 16:57:43 +0200

    Version mit Fehler wegen Test

--- LostInDathe/src/main/GamePanel.java
+++ LostInDathe/src/main/GamePanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
@@ -19,14 +20,25 @@ public class GamePanel extends JPanel implements Runnable{
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow;
 	
+	// FPS
+	int FPS = 60;
+	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;				//Thread ist nötig damit das Spiel durchgehend läuft
 	
+	
+	//Default Spieler Position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	
 	public GamePanel() {			//GamePanel Compiler
 		
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.BLACK);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 
 	public void startGameThread() {				//Thread Compiler
@@ -36,22 +48,83 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	@Override
-	public void run() {
+	/* public void run() {
+		
+		double drawIntervall = 1000000000/FPS; // 0,016666666 Sekunden
+		double nextDrawTime = System.nanoTime() + drawIntervall;
 		
 		while(gameThread != null) {
 			
-			
-//			System.out.println("DerGameloop funktioniert");
-			
-			// 1 UPDATE: update information
 			update();
 			
-			// 2 DRAW: draw the screen with the updated Information
 			repaint();
+			
+			try {
+				
+			    double remainingTime = nextDrawTime - System.nanoTime();
+			    remainingTime = remainingTime/1000000;
+			    
+			    if(remainingTime < 0) {
+			    	remainingTime = 0;
+			    }
+			    
+				Thread.sleep((long) remainingTime);;
+				
+				nextDrawTime += drawIntervall;
+				
+			} catch (InterruptedException e) {
+			    e.printStackTrace();
+			} 
 		}
 	}
-	public void update() {
+	*/
+	public void run() {
 		
+		double drawIntervall = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		//long timer = 0;
+		//int drawCount = 0;
+		
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime)/drawIntervall;
+			//timer += (currentTime-lastTime);
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				//drawCount++;
+			}
+			
+			/* if(timer >= 1000000000) {
+				System.out.println("FPS:" + drawCount);
+				drawCount = 0;
+				timer = 0;
+			} */
+			
+		}
+		
+	}
+	public void update() {							//Darf der Charakter schräg laufen?
+		
+		if(keyH.upPressed == true) {
+			playerY = playerY - playerSpeed;
+		}
+		if(keyH.leftPressed == true) {
+			playerX = playerX - playerSpeed;
+		}
+		if(keyH.downPressed == true) {
+			playerY = playerY + playerSpeed;
+		}
+		if(keyH.rightPressed == true) {
+			playerX = playerX + playerSpeed;
+		}
 	}
 	public void paintComponent(Graphics g) {
 		
@@ -59,21 +132,20 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		Graphics2D g2 = (Graphics2D)g;
 		
-		g2.setColor(Color.WHITE);
+		ImageIcon icon = new ImageIcon("images/Charakter.png");
 		
-		g2.fillRect(100, 100, tileSize, tileSize);
+	    
+		
+		g2.drawImage(icon.getImage(), 100, 100, this);
+		//g2.setColor(Color.WHITE);
+		
+		//g2.fillRect(playerX, playerY, tileSize, tileSize);
 		
 		g2.dispose();
 	}
 }
 
-/*  try {
-			    int secondsToSleep = 1;					//Delay für Gameloop Test
-				Thread.sleep(secondsToSleep * 100);
-			} catch (InterruptedException ie) {
-			    Thread.currentThread().interrupt();
-			} 
-*/
+
 
 
 
