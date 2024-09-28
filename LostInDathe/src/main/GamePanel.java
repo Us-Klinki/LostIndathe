2854cc205cd682eb28c9dde318cb172e4d2ac461
Tom <Tom@Lalilula.fritz.box>
2024-09-28 15:06:18 +0200

    Update

--- LostInDathe/src/main/GamePanel.java
+++ LostInDathe/src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -36,7 +38,42 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		
-		//GameLoop muss erstellt werden
+		while(gameThread != null) {
+			
+			
+//			System.out.println("DerGameloop funktioniert");
+			
+			// 1 UPDATE: update information
+			update();
+			
+			// 2 DRAW: draw the screen with the updated Information
+			repaint();
+		}
+	}
+	public void update() {
 		
 	}
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.WHITE);
+		
+		g2.fillRect(100, 100, tileSize, tileSize);
+		
+		g2.dispose();
+	}
 }
+
+/*  try {
+			    int secondsToSleep = 1;					//Delay für Gameloop Test
+				Thread.sleep(secondsToSleep * 100);
+			} catch (InterruptedException ie) {
+			    Thread.currentThread().interrupt();
+			} 
+*/
+
+
+
