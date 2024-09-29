425c8abf998f2e9527d0d208e3fb63285b5b2fbc
anniphung <thuan.phung728@gmail.com>
2024-09-29 22:36:02 +0200

    Test CollisionChecker

---
+++ LostInDathe/src/main/CollisionChecker.java
@@ -0,0 +1,20 @@
+package main;
+
+public class CollisionChecker {
+	
+	GamePanel gp;
+	
+	
+	
+	private CollisionChecker (GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	private void checkTile(Entity entity) {
+
+	}
+	
+	public void setCollisionChecker(CollisionChecker) {
+		
+	}
+}
