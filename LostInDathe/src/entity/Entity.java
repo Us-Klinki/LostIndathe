892c4d72d9e3c03d1625a921d5599dbbb5f7f521
Lisnatat <108236617+Lisnatat@users.noreply.github.com>
2024-09-29 21:16:06 +0200

    naja

--- LostInDathe/src/entity/Entity.java
+++ LostInDathe/src/entity/Entity.java
@@ -1,15 +1,23 @@
 package entity;
 
+import entity.Player;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+import tile.TileManager;
 
 public class Entity {
 
-	public int worldX, worldY;
-	public int speed;
-	
-	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2, tright1, tright2, tleft1, tleft2, bright1, bright2, bleft1, bleft2;
-	public String direction;
-	
-	public int spriteCounter = 0;
-	public int spriteNum = 1;
+  public int worldX, worldY;
+  public int speed;
+  
+  public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2, tright1, tright2, tleft1, tleft2, bright1, bright2, bleft1, bleft2;
+  public String direction;
+  
+  public int spriteCounter = 0;
+  public int spriteNum = 1;
+
 }
--- LostInDathe/src/main/Main.java
+++ LostInDathe/src/main/Main.java
@@ -1,26 +1,33 @@
 package main;
 
+import entity.Player;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import tile.TileManager;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		JFrame window = new JFrame();								//JFrame wird erstellt
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//Das Fenster kann geschlossen werden
-		window.setResizable(true);									//Das Fenster kann nicht vergrößert werden
-		window.setTitle("Lost in Dathe");							
-		
-		GamePanel gamePanel = new GamePanel();						//GamePanel wird in Main Klasse eingefügt
-		window.add(gamePanel);
-		
-		window.pack();
-		
-		window.setLocationRelativeTo(null);							//Fenster wird in der Mitte des Bildschirms erstellt
-		window.setVisible(true);
-		
-		gamePanel.startGameThread();
-		
-	}
+  
+  public static void main(String[] args) {
+    
+    JFrame window = new JFrame();               //JFrame wird erstellt
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Das Fenster kann geschlossen werden
+    window.setResizable(true);                  //Das Fenster kann nicht vergrößert werden
+    window.setTitle("Lost in Dathe");             
+    
+    GamePanel gamePanel = new GamePanel();            //GamePanel wird in Main Klasse eingefügt
+    window.add(gamePanel);
+    
+    window.pack();
+    
+    window.setLocationRelativeTo(null);             //Fenster wird in der Mitte des Bildschirms erstellt
+    window.setVisible(true);
+    
+    gamePanel.startGameThread();
+    
+  }
 
 }
