c3d558f939badabe393e5de7a500f79b56e7a22f
Lisnatat <108236617+Lisnatat@users.noreply.github.com>
2024-10-06 22:27:16 +0200

    Java Compiler auf SE-8 gesetzt - Export geht jetzt

--- LostInDathe/src/main/Main.java
+++ LostInDathe/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
@@ -24,6 +25,7 @@ public class Main {
     getWindow().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Das Fenster kann geschlossen werden
     getWindow().setResizable(false);                  //Das Fenster kann nicht vergrößert werden
     getWindow().setTitle("Lost in Dathe"); 
+    new Main().setIcon();
     //GUI Leiste oben zum Fenster schließen
     
     
@@ -43,6 +45,12 @@ public class Main {
     gamePanel.setupGame();
     gamePanel.startGameThread();
     
+  }
+  
+  public void setIcon() {
+	  
+	  ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("pictures/piktogramm.png"));
+	  window.setIconImage(icon.getImage());
   }
 
 
