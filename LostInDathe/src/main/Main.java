ee3b7a83267fe87738e9478ab47c0644fa78fb42
Lisnatat <108236617+Lisnatat@users.noreply.github.com>
2024-11-28 01:10:11 +0100

    Main Titel

--- LostInDathe/src/main/Main.java
+++ LostInDathe/src/main/Main.java
@@ -26,7 +26,7 @@ public class Main {
     setWindow(new JFrame());               //JFrame wird erstellt
     getWindow().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Das Fenster kann geschlossen werden
     getWindow().setResizable(false);                  //Das Fenster kann nicht vergrößert werden
-    getWindow().setTitle("Lost in Dathe"); 
+    getWindow().setTitle("Lost in Dathe Version alpha-0.9.0 / 20241127"); 
     new Main().setIcon();
     //GUI Leiste oben zum Fenster schließen
     
